40aa51d8663ee1bed60f20cf2ba5509ac9594262
Kaique Araujo <kaiquematosdearaujo@hotmail.com>
2022-03-25 09:39:28 -0300

    Links do Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,7 +12,7 @@ function Header(props) {
             <header>
                 <nav className="navbar navbar-expand-lg navbar-dark headerNavBar ">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#"><img src={Logo} alt="Logo" width="150 px" /></a>
+                        <a className="navbar-brand" ><Link to="/"><img src={Logo} alt="Logo" width="150 px" /> </Link></a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -27,14 +27,14 @@ function Header(props) {
                                     </form>
                                 </li>
                                 <li className="nav-item col-lg-2 col-md-6 col-4">
-                                  <MiniCartButton totalCart="9.900,00"/>
+                                <Link to="/cart"> <MiniCartButton totalCart="9.900,00"/></Link>
                                 </li>
                                 <li className="nav-item   col-xl-2 col-lg-3 col-md-5 col-7">
                                     <div className="row navLinks">
                                         <img src={BabyUser} class="babyPngUser col-1" />
-                                        <a className="nav-link active col-xl-6 col-lg-5 col-md-4 col-sm-3 col-7" aria-current="page" href="#"> Cadastre-se </a>
+                                        <a className="nav-link active col-xl-6 col-lg-5 col-md-4 col-sm-3 col-7" aria-current="page" ><Link to="../register"> Cadastre-se</Link> </a>
                                         <div className="supportphrase col-2">ou</div>
-                                        <a className="nav-link active col-xl-7 col-lg-6 col-md-5 col-sm-4 col-7 loginLink" aria-current="page" href="#">  Faça seu Login</a>
+                                        <a className="nav-link active col-xl-7 col-lg-6 col-md-5 col-sm-4 col-7 loginLink" aria-current="page"> <Link to="../login">  Faça seu Login</Link></a>
                                     </div>
                                 </li>
 
